c6a4130541457045dd3e2367e6bc9eb0ba24a1d5
shriramg <gshriram14@gmail.com>
2023-03-02 11:52:48 +0530

    That's A Wrap

--- models/pdffile.py
+++
@@ -1,5 +0,0 @@
-from PyPDF2 import PdfReader
-text = PdfReader('todo_dummy_merged.pdf')
-length = len(text.pages)
-text = ''.join([text.pages[i].extract_text() for i in range(length)])
-print(text)
--- models/testsklearn.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-geometrymodel = pickle.load(open('geometrymodel','rb'))
-geometryvectorizer = pickle.load(open('geometryvectorizer','rb'))
-text = 'lightweight shock resistant material'
-geometrymodel.predict(geometryvectorizer.transform([text]))
